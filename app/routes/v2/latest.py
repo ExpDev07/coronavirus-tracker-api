89ea4ff7f163cc3a30b0a94466297335f53c9287
SeanCena <seansng1@gmail.com>
2020-03-21 00:26:33 -0700

    Added option for aggregate data for countries

--- app/routes/v2/latest.py
+++ app/routes/v2/latest.py
@@ -1,11 +1,18 @@
-from flask import jsonify
+from flask import jsonify, request
 from ...routes import api_v2 as api
 from ...services import jhu
 
 @api.route('/latest')
 def latest():
+    #Query parameters.
+    country_code = request.args.get('country_code', type=str)
+
     # Get the serialized version of all the locations.
     locations = [ location.serialize() for location in jhu.get_all() ]
+
+    # Return aggregate data for country if provided.
+    if not country_code is None:
+        locations = list(filter(lambda location: location['country_code'] == country_code.upper(), locations))
 
     # All the latest information.
     latest = list(map(lambda location: location['latest'], locations))
