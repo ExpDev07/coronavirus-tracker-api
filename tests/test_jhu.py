c1a5c522f1422d9cbc210b9704a178ee65603203
ExpDev07 <marius.richardsen@hiof.no>
2020-03-24 05:52:55 +0100

    works

--- tests/test_jhu.py
+++ tests/test_jhu.py
@@ -27,7 +27,7 @@ def mocked_requests_get(*args, **kwargs):
             state = state.lower()
 
             # Determine filepath.
-            filepath = "tests/example_data/{}".format(state)
+            filepath = "tests/example_data/{}.csv".format(state)
 
             if state == 'recovered':
                 filepath = 'tests/example_data/time_series_19-covid-Recovered.csv'
