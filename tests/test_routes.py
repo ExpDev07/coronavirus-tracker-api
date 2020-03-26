d46435150818ff0bca3007c1db9242488195ad19
Gabriel <gabriel59kg@gmail.com>
2020-03-25 22:25:15 -0400

    404 for missing location data

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -129,7 +129,7 @@ class FlaskRoutesTest(unittest.TestCase):
         ({"timelines": 1}, 200),
         ({"source": "jhu", "timelines": True}, 200),
         ({"source": "csbs", "country_code": "US"}, 200),
-        ({"source": "jhu", "country_code": "US"}, 404)
+        ({"source": "jhu", "country_code": "US"}, 404),
     ],
 )
 def test_locations_status_code(api_client, query_params, expected_status):
@@ -137,6 +137,7 @@ def test_locations_status_code(api_client, query_params, expected_status):
     print(f"GET {response.url}\n{response}")
     print(f"\tjson:\n{pf(response.json())[:1000]}\n\t...")
     assert response.status_code == expected_status
+
 
 @pytest.mark.parametrize(
     "query_params",
