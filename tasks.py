c6e36179fb2b91aa297eaa54940672b03916e903
Gabriel <gabriel59kg@gmail.com>
2020-04-10 21:28:05 -0400

    Docker fix & invoke tasks (#13)
    * Use gunicorn
    
    * add docker build + run invoke tasks

--- tasks.py
+++ tasks.py
@@ -9,6 +9,8 @@ Available commands
   invoke sort
   invoke check
 """
+import random
+
 import invoke
 
 TARGETS_DESCRIPTION = "Paths/directories to format. [default: . ]"
@@ -71,3 +73,17 @@ def generate_reqs(ctx):
     """Generate requirements.txt"""
     reqs = ["pipenv lock -r > requirements.txt", "pipenv lock -r --dev > requirements-dev.txt"]
     [ctx.run(req) for req in reqs]
+
+
+@invoke.task
+def docker(
+    ctx, build=False, run=False, tag="covid-tracker-api:latest", port=80, name=f"covid-api-{random.randint(0,999)}"
+):  # pylint: disable=too-many-arguments
+    """Build and run docker container."""
+    if not any([build, run]):
+        raise invoke.Exit(message="Specify either --build or --run", code=1)
+    if build:
+        docker_cmds = ["build", "."]
+    else:
+        docker_cmds = ["run", "-p", str(port), "--name", name]
+    ctx.run(" ".join(["docker", *docker_cmds, "-t", tag]))
