f5605c2e45974da6a0a4a15083bdf1229916ab1d
Gabriel <gabriel59kg@gmail.com>
2020-04-10 21:57:04 -0400

    hardcode published port (#14)

--- tasks.py
+++ tasks.py
@@ -76,14 +76,12 @@ def generate_reqs(ctx):
 
 
 @invoke.task
-def docker(
-    ctx, build=False, run=False, tag="covid-tracker-api:latest", port=80, name=f"covid-api-{random.randint(0,999)}"
-):  # pylint: disable=too-many-arguments
+def docker(ctx, build=False, run=False, tag="covid-tracker-api:latest", name=f"covid-api-{random.randint(0,999)}"):
     """Build and run docker container."""
     if not any([build, run]):
         raise invoke.Exit(message="Specify either --build or --run", code=1)
     if build:
         docker_cmds = ["build", "."]
     else:
-        docker_cmds = ["run", "-p", str(port), "--name", name]
+        docker_cmds = ["run", "--publish", "80", "--name", name]
     ctx.run(" ".join(["docker", *docker_cmds, "-t", tag]))
