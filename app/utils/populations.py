f16df566c9c13d0c91bfda65ec6591203f4d5e17
Gabriel <gabriel59kg@gmail.com>
2020-04-18 12:47:08 -0400

    use io module to save and load poplutation data backups

--- app/utils/populations.py
+++ app/utils/populations.py
@@ -1,9 +1,13 @@
 """app.utils.populations.py"""
+import json
 import logging
 
 import requests
 
+import app.io
+
 LOGGER = logging.getLogger(__name__)
+GEONAMES_BACKUP_PATH = "geonames_population_mappings.json"
 
 # Fetching of the populations.
 def fetch_populations():
@@ -20,12 +24,20 @@ def fetch_populations():
     mappings = {}
 
     # Fetch the countries.
-    countries = requests.get("http://api.geonames.org/countryInfoJSON?username=dperic").json()["geonames"]
+    try:
+        countries = requests.get(
+            "http://api.geonames.org/countryInfoJSON", params={"username": "dperic"}, timeout=2
+        ).json()["geonames"]
+        # Go through all the countries and perform the mapping.
+        for country in countries:
+            mappings.update({country["countryCode"]: int(country["population"]) or None})
 
-    # Go through all the countries and perform the mapping.
-    for country in countries:
-        mappings.update({country["countryCode"]: int(country["population"]) or None})
-
+        if mappings:
+            app.io.save(GEONAMES_BACKUP_PATH, mappings)
+    except (json.JSONDecodeError, KeyError, requests.exceptions.Timeout) as err:
+        LOGGER.warning(f"Error pulling population data. {err.__class__.__name__}: {err}")
+        mappings = app.io.load(GEONAMES_BACKUP_PATH)
+        LOGGER.info(f"Using backup data from {GEONAMES_BACKUP_PATH}")
     # Finally, return the mappings.
     return mappings
 
