a58c508b057c0dc0a549907657645921bd7b557a
Gabriel <gabriel59kg@gmail.com>
2020-04-18 16:34:04 -0400

    don't write to file-system by default

--- app/utils/populations.py
+++ app/utils/populations.py
@@ -11,10 +11,12 @@ GEONAMES_URL = "http://api.geonames.org/countryInfoJSON"
 GEONAMES_BACKUP_PATH = "geonames_population_mappings.json"
 
 # Fetching of the populations.
-def fetch_populations():
+def fetch_populations(save=False):
     """
     Returns a dictionary containing the population of each country fetched from the GeoNames.
     https://www.geonames.org/
+
+    TODO: only skip writing to the filesystem when deployed with gunicorn, or handle concurent access, or use DB.
 
     :returns: The mapping of populations.
     :rtype: dict
@@ -26,13 +28,13 @@ def fetch_populations():
 
     # Fetch the countries.
     try:
-        countries = requests.get(GEONAMES_URL, params={"username": "dperic"}, timeout=2).json()["geonames"]
+        countries = requests.get(GEONAMES_URL, params={"username": "dperic"}, timeout=1.5).json()["geonames"]
         # Go through all the countries and perform the mapping.
         for country in countries:
             mappings.update({country["countryCode"]: int(country["population"]) or None})
 
-        if mappings:
-            app.io.save(GEONAMES_BACKUP_PATH, mappings)
+        if mappings and save:
+            LOGGER.info(f"Saving population data to {app.io.save(GEONAMES_BACKUP_PATH, mappings)}")
     except (json.JSONDecodeError, KeyError, requests.exceptions.Timeout) as err:
         LOGGER.warning(f"Error pulling population data. {err.__class__.__name__}: {err}")
         mappings = app.io.load(GEONAMES_BACKUP_PATH)
--- tests/test_populations.py
+++ tests/test_populations.py
@@ -3,6 +3,7 @@ import pytest
 import requests.exceptions
 import responses
 
+import app.io
 import app.utils.populations
 
 NOT_FOUND_HTML = """<!DOCTYPE html>
@@ -16,11 +17,59 @@ NOT_FOUND_HTML = """<!DOCTYPE html>
 </body>
 </html>"""
 
+SAMPLE_GEONAMES_JSON = {
+    "geonames": [
+        {
+            "continent": "EU",
+            "capital": "Andorra la Vella",
+            "languages": "ca",
+            "geonameId": 3041565,
+            "south": 42.42874300100004,
+            "isoAlpha3": "AND",
+            "north": 42.65576500000003,
+            "fipsCode": "AN",
+            "population": "77006",
+            "east": 1.786576000000025,
+            "isoNumeric": "020",
+            "areaInSqKm": "468.0",
+            "countryCode": "AD",
+            "west": 1.413760001000071,
+            "countryName": "Andorra",
+            "continentName": "Europe",
+            "currencyCode": "EUR",
+        },
+        {
+            "continent": "AS",
+            "capital": "Abu Dhabi",
+            "languages": "ar-AE,fa,en,hi,ur",
+            "geonameId": 290557,
+            "south": 22.6315119400001,
+            "isoAlpha3": "ARE",
+            "north": 26.0693916590001,
+            "fipsCode": "AE",
+            "population": "9630959",
+            "east": 56.381222289,
+            "isoNumeric": "784",
+            "areaInSqKm": "82880.0",
+            "countryCode": "AE",
+            "west": 51.5904085340001,
+            "countryName": "United Arab Emirates",
+            "continentName": "Asia",
+            "currencyCode": "AED",
+        },
+    ]
+}
+
 
 @responses.activate
 @pytest.mark.parametrize(
     "body_arg, json_arg",
-    [(NOT_FOUND_HTML, None), (None, {"foo": "bar"}), (requests.exceptions.Timeout("Forced Timeout"), None)],
+    [
+        (None, SAMPLE_GEONAMES_JSON),
+        (NOT_FOUND_HTML, None),
+        (None, {"foo": "bar"}),
+        (requests.exceptions.Timeout("Forced Timeout"), None),
+    ],
 )
 def test_fetch_populations(body_arg, json_arg):
     responses.add(responses.GET, app.utils.populations.GEONAMES_URL, body=body_arg, json=json_arg)
