d94e0c5f3a85af1af44a824357be4868f201b7e2
Hassan Nisar <hassan82@my.yorku.ca>
2021-07-21 21:29:04 -0400

    added utils aggregate to improve code design

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -32,11 +32,11 @@ class Utils:
         """
         Return whether the string can be interpreted as a date.
         - https://stackoverflow.com/a/25341965/7120095
-    
+
         :param string: str, string to check for date
         :param fuzzy: bool, ignore unknown tokens in string if True
         """
-    
+
         try:
             parse(string, fuzzy=fuzzy)
             return True
