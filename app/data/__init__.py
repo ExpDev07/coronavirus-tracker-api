a0dffbd946b23970d57cc0caf710607527e03adc
Solracion <csoto_@outlook.com>
2021-07-22 00:24:38 -0400

    Data_Source change for a more Aggregate approach

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -6,12 +6,13 @@ from ..services.location import LocationService
 
 # Mapping of services to data-sources.
 class DATA_SOURCES:
-    DATA_SOURCES = {}
+    data_SOURCES = {}
 
-    def data(self)
-    self.DATA_SOURCES['jhu'] = JhuLocationService()
-    self.DATA_SOURCES['csbs'] = CSBSLocationService()
-    self.DATA_SOURCES['nyt'] = NYTLocationService()
+    def data(self):
+        self.data_SOURCES['jhu'] = JhuLocationService()
+        self.data_SOURCES['csbs'] = CSBSLocationService()
+        self.data_SOURCES['nyt'] = NYTLocationService()
 
-    def data_source(self, source : str)-> LocationService:
-         return self.DATA_SOURCES.get(source.lower())
+    def data_source(self, source: str)-> LocationService:
+
+         return self.data_SOURCES.get(source.lower())
