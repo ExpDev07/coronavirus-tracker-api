330b21282418bf4fbd31cd723d1552918757c410
Simran Sandhu <simran7s@my.yorku.ca>
2021-07-23 00:43:18 -0400

    modified httputils.py to incorporate an aggregrate

--- app/utils/httputils.py
+++ app/utils/httputils.py
@@ -3,28 +3,26 @@ import logging
 
 from aiohttp import ClientSession
 
-# Singleton aiohttp.ClientSession instance.
-CLIENT_SESSION: ClientSession
 
+class Session:
+    # Singleton aiohttp.ClientSession instance.
+    CLIENT_SESSION: ClientSession
+    LOGGER = logging.getLogger(__name__)
 
-LOGGER = logging.getLogger(__name__)
+    async def setup_client_session(self):
+        """Set up the application-global aiohttp.ClientSession instance.
 
+        aiohttp recommends that only one ClientSession exist for the lifetime of an application.
+        See: https://docs.aiohttp.org/en/stable/client_quickstart.html#make-a-request
 
-async def setup_client_session():
-    """Set up the application-global aiohttp.ClientSession instance.
+        """
+        global CLIENT_SESSION  # pylint: disable=global-statement
+        self.LOGGER.info("Setting up global aiohttp.ClientSession.")
+        CLIENT_SESSION = ClientSession()
 
-    aiohttp recommends that only one ClientSession exist for the lifetime of an application.
-    See: https://docs.aiohttp.org/en/stable/client_quickstart.html#make-a-request
-
-    """
-    global CLIENT_SESSION  # pylint: disable=global-statement
-    LOGGER.info("Setting up global aiohttp.ClientSession.")
-    CLIENT_SESSION = ClientSession()
-
-
-async def teardown_client_session():
-    """Close the application-global aiohttp.ClientSession.
-    """
-    global CLIENT_SESSION  # pylint: disable=global-statement
-    LOGGER.info("Closing global aiohttp.ClientSession.")
-    await CLIENT_SESSION.close()
+    async def teardown_client_session(self):
+        """Close the application-global aiohttp.ClientSession.
+        """
+        global CLIENT_SESSION  # pylint: disable=global-statement
+        self.LOGGER.info("Closing global aiohttp.ClientSession.")
+        await CLIENT_SESSION.close()
