9fe320b2af22f119ba3858706b505241c2faa02d
Simran Sandhu <simran7s@my.yorku.ca>
2021-07-24 17:03:07 -0400

    updated test file to work with aggregate pattern

--- tests/test_httputils.py
+++ tests/test_httputils.py
@@ -9,11 +9,13 @@ async def test_setup_teardown_client_session():
         # Ensure client_session is undefined prior to setup
         httputils.CLIENT_SESSION
 
-    await httputils.setup_client_session()
+    testSesssion = httputils.Session()
+
+    await testSesssion.setup_client_session()
 
     assert httputils.CLIENT_SESSION
 
-    await httputils.teardown_client_session()
+    await testSesssion.teardown_client_session()
     assert httputils.CLIENT_SESSION.closed
 
     del httputils.CLIENT_SESSION
