ee81d72aebf0c11a058f6491dc7b50a3e71ca83e
Simran Sandhu <simran7s@my.yorku.ca>
2021-07-24 17:08:45 -0400

    added Logger set method

--- app/utils/httputils.py
+++ app/utils/httputils.py
@@ -21,6 +21,9 @@ class Session:
     def reassignClientSession(self):
         self.__CLIENT_SESSION = CLIENT_SESSION  # reassign to updated CLIENT_SESSION
 
+    def reassignLogger(self):
+        self.__LOGGER = logging.getLogger(__name__)
+
     async def setup_client_session(self):
         """Set up the application-global aiohttp.ClientSession instance.
 
@@ -29,14 +32,16 @@ class Session:
 
         """
         global CLIENT_SESSION  # pylint: disable=global-statement
+        self.reassignLogger()
         self.__LOGGER.info("Setting up global aiohttp.ClientSession.")
         CLIENT_SESSION = ClientSession()
-        self.reassignClientSession(self)
+        self.reassignClientSession()
 
     async def teardown_client_session(self):
         """Close the application-global aiohttp.ClientSession.
         """
         global CLIENT_SESSION  # pylint: disable=global-statement
+        self.reassignLogger()
         self.__LOGGER.info("Closing global aiohttp.ClientSession.")
         await CLIENT_SESSION.close()
         self.reassignClientSession(self)
