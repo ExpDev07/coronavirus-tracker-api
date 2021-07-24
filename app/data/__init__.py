921eda05bd1c07a9bab88e988ebecf168b3eeb80
Jae <arthur.choi2014@gmail.com>
2021-07-24 15:45:05 +0900

    Applying Aggregate in the API

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -3,19 +3,24 @@ from ..services.location.csbs import CSBSLocationService
 from ..services.location.jhu import JhuLocationService
 from ..services.location.nyt import NYTLocationService
 
+
 # Mapping of services to data-sources.
-DATA_SOURCES = {
-    "jhu": JhuLocationService(),
-    "csbs": CSBSLocationService(),
-    "nyt": NYTLocationService(),
-}
+class DataSources:
+    data_sources = {}
+
+    def __init__(self):
+        self.data_sources = {
+            "jhu": JhuLocationService(),
+            "csbs": CSBSLocationService(),
+            "nyt": NYTLocationService(),
+        }
 
 
-def data_source(source):
+def data_source(self, source):
     """
     Retrieves the provided data-source service.
 
     :returns: The service.
     :rtype: LocationService
     """
-    return DATA_SOURCES.get(source.lower())
+    return self.data_sources.get(source.lower())
