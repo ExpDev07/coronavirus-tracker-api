9e44f6de36d3374f15186f059c068415b168756c
emlee518 <emlee518@gmail.com>
2021-07-23 21:52:46 -0400

    Applied aggregation to the data sources and updated how they can be called in main.py.

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -4,18 +4,21 @@ from ..services.location.jhu import JhuLocationService
 from ..services.location.nyt import NYTLocationService
 
 # Mapping of services to data-sources.
-DATA_SOURCES = {
-    "jhu": JhuLocationService(),
-    "csbs": CSBSLocationService(),
-    "nyt": NYTLocationService(),
-}
+class DATASOURCES:
+    __data_sources = {}
+    def __init__(self):
+        self.__date_sources = {
+            "jhu": JhuLocationService(),
+            "csbs": CSBSLocationService(),
+            "nyt": NYTLocationService(),
+        }
 
 
-def data_source(source):
+def get_data_sources(self, source: str):
     """
     Retrieves the provided data-source service.
 
     :returns: The service.
     :rtype: LocationService
     """
-    return DATA_SOURCES.get(source.lower())
+    return self.__data_sources.get(source.lower())
