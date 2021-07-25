8e078fe95962dbd7628b0bc6444a152b408e3d11
anjilag <anjilag@my.yorku.ca>
2021-07-24 21:54:32 -0400

    Applied aggregate pattern to countries.py

--- app/utils/countries.py
+++ app/utils/countries.py
@@ -366,15 +366,20 @@ COUNTRY_NAME__COUNTRY_CODE = {
     # "MS Zaandam"
 }
 
-# fmt: on
-def country_code(value):
-    """
-    Return two letter country code (Alpha-2) according to https://en.wikipedia.org/wiki/ISO_3166-1
-    Defaults to "XX".
-    """
-    code = COUNTRY_NAME__COUNTRY_CODE.get(value, DEFAULT_COUNTRY_CODE)
-    if code == DEFAULT_COUNTRY_CODE:
-        # log at sub DEBUG level
-        LOGGER.log(5, f"No country code found for '{value}'. Using '{code}'!")
+class Country: 
 
-    return code
+    def __init__(self, name):
+        self.name = name
+
+    # fmt: on
+    def country_code(value):
+        """
+        Return two letter country code (Alpha-2) according to https://en.wikipedia.org/wiki/ISO_3166-1
+        Defaults to "XX".
+        """
+        code = COUNTRY_NAME__COUNTRY_CODE.get(value, DEFAULT_COUNTRY_CODE)
+        if code == DEFAULT_COUNTRY_CODE:
+            # log at sub DEBUG level
+            LOGGER.log(5, f"No country code found for '{value}'. Using '{code}'!")
+
+        return code
