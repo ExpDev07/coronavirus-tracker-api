d42f4f5811f0dce657145b70de4b42fcafbe2bfa
anurosan <56799479+anurosan@users.noreply.github.com>
2021-07-24 23:36:54 -0400

    Update __init__.py

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -3,7 +3,7 @@ from ..services.location.csbs import CSBSLocationService
 from ..services.location.jhu import JhuLocationService
 from ..services.location.nyt import NYTLocationService
 
-class Data()
+class Data
     def __init__(self.source)
     # Mapping of services to data-sources.
         DATA_SOURCES = {
