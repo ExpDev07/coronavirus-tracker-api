efb865eb2585ecf91213df76f1c5ec14c25dfa57
anurosan <56799479+anurosan@users.noreply.github.com>
2021-07-24 23:52:20 -0400

    Fixed Init

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -3,8 +3,8 @@ from ..services.location.csbs import CSBSLocationService
 from ..services.location.jhu import JhuLocationService
 from ..services.location.nyt import NYTLocationService
 
-class Data
-    def __init__(self.source)
+class Data:
+    def __init__(self, source)
     # Mapping of services to data-sources.
         DATA_SOURCES = {
             "jhu": JhuLocationService(),
