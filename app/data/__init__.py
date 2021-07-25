97112b47191d73397fc35aea4275db1681804a7b
anurosan <56799479+anurosan@users.noreply.github.com>
2021-07-24 23:22:21 -0400

    Update __init__.py

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -3,19 +3,21 @@ from ..services.location.csbs import CSBSLocationService
 from ..services.location.jhu import JhuLocationService
 from ..services.location.nyt import NYTLocationService
 
-# Mapping of services to data-sources.
-DATA_SOURCES = {
-    "jhu": JhuLocationService(),
-    "csbs": CSBSLocationService(),
-    "nyt": NYTLocationService(),
-}
+class Data()
+    def __init__(self.source)
+    # Mapping of services to data-sources.
+    DATA_SOURCES = {
+        "jhu": JhuLocationService(),
+        "csbs": CSBSLocationService(),
+        "nyt": NYTLocationService(),
+    }
 
 
-def data_source(source):
-    """
-    Retrieves the provided data-source service.
+    def data_source(self):
+        """
+        Retrieves the provided data-source service.
 
-    :returns: The service.
-    :rtype: LocationService
-    """
-    return DATA_SOURCES.get(source.lower())
+        :returns: The service.
+        :rtype: LocationService
+        """
+        return self.DATA_SOURCES.get(source.lower())
