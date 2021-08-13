5fa3f265dbabd3fe341707a303027809fab35403
Solracion <csoto_@outlook.com>
2021-08-13 01:04:55 -0400

    Testing Composite

--- app/coordinates.py
+++ app/coordinates.py
@@ -1,23 +1,23 @@
 """app.coordinates.py"""
 
-
-class Coordinates:
-    """
-    A position on earth using decimal coordinates (latitude and longitude).
-    """
-
+class Position(object):
     def __init__(self, latitude, longitude):
         self.latitude = latitude
         self.longitude = longitude
 
-    def serialize(self):
+class Coordinates(Position):
+    """
+    A position on earth using decimal coordinates (latitude and longitude).
+    """
+    def serialize(self, position):
+        self.position = {"latitude": self.latitude, "longitude": self.longitude}
         """
         Serializes the coordinates into a dict.
 
         :returns: The serialized coordinates.
         :rtype: dict
         """
-        return {"latitude": self.latitude, "longitude": self.longitude}
+        return self.postion
 
     def __str__(self):
         return "lat: %s, long: %s" % (self.latitude, self.longitude)
