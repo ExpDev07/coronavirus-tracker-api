2248aff6ab7f44b20f3f344cb56b17802b04db0d
Ali Tayyabi <ali@MacBook-Pro.local>
2021-08-15 14:12:08 -0400

    creational pattern patch

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -3,14 +3,6 @@ from ..services.location.csbs import CSBSLocationService
 from ..services.location.jhu import JhuLocationService
 from ..services.location.nyt import NYTLocationService
 
-# Mapping of services to data-sources.
-DATA_SOURCES = {
-    "jhu": JhuLocationService(),
-    "csbs": CSBSLocationService(),
-    "nyt": NYTLocationService(),
-}
-
-
 def data_source(source):
     """
     Retrieves the provided data-source service.
@@ -18,4 +10,13 @@ def data_source(source):
     :returns: The service.
     :rtype: LocationService
     """
-    return DATA_SOURCES.get(source.lower())
+    s = source.lower()
+    # return the singleton instance of the required service
+    if s == 'jhu': 
+        return JhuLocationService.getInstance()
+    elif s == 'csbs': 
+        return CSBSLocationService.getInstance()
+    elif s == 'nyt': 
+        return NYTLocationService.getInstance()
+    else:
+        return None
--- app/services/location/__init__.py
+++ app/services/location/__init__.py
@@ -7,6 +7,10 @@ class LocationService(ABC):
     Service for retrieving locations.
     """
 
+    # this class variable will store the singleton instance for 
+    # the LocationService instance from the child class
+    __instance__ = None
+
     @abstractmethod
     async def get_all(self):
         """
@@ -26,3 +30,13 @@ class LocationService(ABC):
         :rtype: Location
         """
         raise NotImplementedError
+    
+    @classmethod
+    def getInstance(cls):
+        # singleton pattern
+        if cls.__instance__ is None:
+            # instantiate new object from the class (note that we are 
+            # expecting 0 parameters for the constructor. if parameters 
+            # needed this method should be overridden by the subclass)
+            cls.__instance__ = cls()
+        return cls.__instance__
