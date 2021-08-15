997ed85dcffe4f24af2a3aba629f123176bf8d1d
hughie_coles <hughie.coles@gmail.com>
2021-08-15 18:23:09 -0230

    add constants for each data source to remove magic strings

--- app/services/location/locationservicefactory.py
+++ app/services/location/locationservicefactory.py
@@ -5,12 +5,17 @@ from ..location.jhu import JhuLocationService
 from ..location.nyt import NYTLocationService
 
 
+NEW_YORK_TIMES = 'nyt'
+JOHNS_HOPKINS_UNIVERSITY = 'jhu'
+CSBS = 'csbs'
+
+
 class LocationServiceFactory:
     def create(location_service_name):
         lowercase_service_name = location_service_name.lower()
-        if lowercase_service_name == 'csbs':
+        if lowercase_service_name == CSBS:
             return CSBSLocationService()
-        elif lowercase_service_name == 'jhu':
+        elif lowercase_service_name == JOHNS_HOPKINS_UNIVERSITY:
             return JhuLocationService()
-        elif lowercase_service_name == 'nyt':
+        elif lowercase_service_name == NEW_YORK_TIMES:
             return NYTLocationService()
