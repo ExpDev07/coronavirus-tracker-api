976b8d41db905f8e0a84ce5a2adcf565c1f9e2e4
Billa32 <87912290+Billa32@users.noreply.github.com>
2021-08-15 21:09:00 -0400

    Update __init__.py

--- app/location/__init__.py
+++ app/location/__init__.py
@@ -82,7 +82,81 @@ class Location(ILocation):  # pylint: disable=too-many-instance-attributes
             },
         }
 
+# pylint: disable=redefined-builtin,invalid-name
+class LocationAdapter(Location):  # pylint: disable=too-many-instance-attributes
+    """
+    A location in the world affected by the coronavirus.
+    """
 
+    def __init__(
+        self, id, country, province, coordinates, last_updated, confirmed, deaths, recovered,
+    ):  # pylint: disable=too-many-arguments
+        # General info.
+        self.id = id
+        self.country = country.strip()
+        self.province = province.strip()
+        self.coordinates = coordinates
+
+        # Last update.
+        self.last_updated = last_updated
+
+        # Statistics.
+        self.confirmed = confirmed
+        self.deaths = deaths
+        self.recovered = recovered
+
+    @property
+    def country_code(self):
+        """
+        Gets the alpha-2 code represention of the country. Returns 'XX' if none is found.
+
+        :returns: The country code.
+        :rtype: str
+        """
+        return (countries.country_code(self.country) or countries.DEFAULT_COUNTRY_CODE).upper()
+
+    @property
+    def country_population(self):
+        """
+        Gets the population of this location.
+
+        :returns: The population.
+        :rtype: int
+        """
+        return country_population(self.country_code)
+
+    def serialize(self):
+        """
+        Serializes the location into a dict.
+
+        :returns: The serialized location.
+        :rtype: dict
+        """
+        serialized =  {
+            # General info.
+            "id": self.id,
+            "country": self.country,
+            "country_code": self.country_code,
+            "country_population": self.country_population,
+            "province": self.province,
+            # Coordinates.
+            "coordinates": self.coordinates.serialize(),
+            # Last updated.
+            "last_updated": self.last_updated,
+            # Latest data (statistics).
+            "latest": {
+                "confirmed": self.confirmed,
+                "deaths": self.deaths,
+                "recovered": self.recovered,
+            },
+        }
+
+        # Update with new fields.
+        serialized.update(
+            {"state": self.province, "county": self.county,}
+        )
+
+        return serialized
 
 class TimelinedLocation(Location):
     """
