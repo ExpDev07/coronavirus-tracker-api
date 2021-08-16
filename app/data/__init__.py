209f36f682e9a61aa510ad1f07b956c9bb4840b1
DukeM23 <dukemaquiling@gmail.com>
2021-08-15 21:38:24 -0600

    EECS 3311: Creational Design Pattern - Singleton

--- app/data/__init__.py
+++ app/data/__init__.py
@@ -3,19 +3,34 @@ from ..services.location.csbs import CSBSLocationService
 from ..services.location.jhu import JhuLocationService
 from ..services.location.nyt import NYTLocationService
 
-# Mapping of services to data-sources.
-DATA_SOURCES = {
-    "jhu": JhuLocationService(),
-    "csbs": CSBSLocationService(),
-    "nyt": NYTLocationService(),
-}
-
-
-def data_source(source):
+class DataSourceSingletonMeta(type):
     """
-    Retrieves the provided data-source service.
-
-    :returns: The service.
-    :rtype: LocationService
+    access point to the DataSourceSingleton
     """
-    return DATA_SOURCES.get(source.lower())
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Possible changes to the value of the `__init__` argument do not affect
+        the returned instance.
+        """
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class DataSourceSingleton(metaclass=DataSourceSingletonMeta):
+        DATA_SOURCES = {
+            "jhu": JhuLocationService(),
+            "csbs": CSBSLocationService(),
+            "nyt": NYTLocationService(),
+        }
+        def get_data_source(self, dataSource):
+            return self.DATA_SOURCES.get(dataSource.lower())
+        ...
+
+        def get_data_source_list(self): 
+            return self.DATA_SOURCES
+
+        
--- app/main.py
+++ app/main.py
@@ -14,7 +14,8 @@ from scout_apm.async_.starlette import ScoutMiddleware
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
 
 from .config import get_settings
-from .data import data_source
+# from .data import data_source
+from .data import DataSourceSingleton
 from .routers import V1, V2
 from .utils.httputils import setup_client_session, teardown_client_session
 
@@ -73,8 +74,12 @@ async def add_datasource(request: Request, call_next):
     """
     Attach the data source to the request.state.
     """
+
+
+
     # Retrieve the datas ource from query param.
-    source = data_source(request.query_params.get("source", default="jhu"))
+    source = DataSourceSingleton().get_data_source(request.query_params.get("source", default="jhu"))
+    # data_source(request.query_params.get("source", default="jhu"))
 
     # Abort with 404 if source cannot be found.
     if not source:
